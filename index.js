cf2b479ef11e5d54868810d78675d8998123b37b
shahil <shahil.shan.s87@kalvium.community>
2025-09-16 10:38:24 +0530

    Connected Express app to MongoDB

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const { resolve } = require('path');
 const app = express();
 const port = 3010;
 
+
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+  
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
